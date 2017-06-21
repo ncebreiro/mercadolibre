3cc80289db8c0c4e4e96e1549c0e69c32b783ff1
Nicolás Cebreiro <nicolas.cebreiro@gmail.com>
2017-06-21 15:46:48 -0300

    Se finalizan las logicas de todas las pantallas

--- routes/items.js
+++ routes/items.js
@@ -9,13 +9,18 @@ router.get('/', function (req, res, next) {
     var search_box_get_parsed = search_box.replace(' ', '+');
     var api_query = 'https://api.mercadolibre.com/sites/MLA/search?q=' + search_box_get_parsed + '&limit=4';
     request.get(api_query, function (err, res, body) {
-        // if(err) {
-        //
-        // }
-        // if(res.statusCode !== 200 ) {
-        // }
-        json_data_result = JSON.parse(body);
-        main_res.render('items', {title: 'Mercadolibre', items: json_data_result, search_box: search_box})
+        var json_data_result = JSON.parse(body);
+        var category_id = json_data_result.results[0].category_id;
+        var category_query = 'https://api.mercadolibre.com/categories/' + category_id;
+        request.get(category_query, function (err, category_res, category_body) {
+            var json_categories_result = JSON.parse(category_body);
+            main_res.render('items', {
+                title: 'Mercadolibre',
+                items: json_data_result,
+                search_box: search_box,
+                categories: json_categories_result
+            })
+        });
     });
 });
 
@@ -25,22 +30,22 @@ router.get('/:id', function (req, res, next) {
     var api_query = 'https://api.mercadolibre.com/items/' + item_id;
     var main_res = res;
     request.get(api_query, function (err, res, body) {
-        // if(err) {
-        //
-        // }
-        // if(res.statusCode !== 200 ) {
-        // }
-        json_data_result = JSON.parse(body);
+        var json_data_result = JSON.parse(body);
         var description_query = 'https://api.mercadolibre.com/items/' + item_id + '/description';
         request.get(description_query, function (err, res, body) {
-            json_description = JSON.parse(body);
-            main_res.render('item_description', {
-                title: 'Mercadolibre',
-                item: json_data_result,
-                item_description: json_description
-            })
+            var json_description = JSON.parse(body);
+            var category_id = json_data_result.category_id;
+            var category_query = 'https://api.mercadolibre.com/categories/' + category_id;
+            request.get(category_query, function (err, category_res, category_body) {
+                var json_categories_result = JSON.parse(category_body);
+                main_res.render('item_description', {
+                    title: 'Mercadolibre',
+                    item: json_data_result,
+                    item_description: json_description,
+                    categories: json_categories_result
+                });
+            });
         });
-
     });
 });
 
